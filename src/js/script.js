bd6b573f84e68007299811eaeef6487890d4aaed
Maks Birecki <maksymilian.birecki@gmail.com>
2024-08-02 15:34:17 +0200

    toEndFirstScript

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,15 @@
 const bookWrapper = document.querySelector('.books-list');
-function renderBooks(){
-    for(const bookId of dataSource.books){
-        const book = dataSource.books[bookId];
-        const name = book.name;
-        const price = book.price;
-        const rating = book.rating;
-        const image = book.image;    
-        bookWrapper.innerHTML = '<li></li>';
-    }
-}
-renderBooks();
+let templateBook = document.querySelector("#template-book").innerHTML;
+const renderBooks = Handlebars.compile(templateBook);
+let tempBookObj1 = {
+    // for (const bookId in dataSource.books) {
+    //     const book = dataSource.books[bookId],
+    //     const name = book.name,
+    //     const price = book.price,
+    //     const rating = book.rating,
+    //     const image = book.image,
+    //   }
+  };
+  
+  let tempBookFromObj1 = renderBooks(tempBookObj1);
+renderBooks()
