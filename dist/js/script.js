04117fff9e00856a71afa9a0d05d09ec97232d04
Maks Birecki <maksymilian.birecki@gmail.com>
2024-08-02 16:44:47 +0200

    addScriptRemoveClass

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,15 +1,40 @@
 const bookWrapper = document.querySelector('.books-list');
-let templateBook = document.querySelector("#template-book").innerHTML;
-const renderBooks = Handlebars.compile(templateBook);
-let tempBookObj1 = {
-    // for (const bookId in dataSource.books) {
-    //     const book = dataSource.books[bookId],
-    //     const name = book.name,
-    //     const price = book.price,
-    //     const rating = book.rating,
-    //     const image = book.image,
-    //   }
-  };
-  
-  let tempBookFromObj1 = renderBooks(tempBookObj1);
-renderBooks()
+const templateBook = document.querySelector('#template-book').innerHTML;
+const render = Handlebars.compile(templateBook);
+function renderBooks(){
+    let booksHTML = '';
+    for(const bookId in dataSource.books){
+        const book = dataSource.books[bookId];
+        const bookHTML = render(book);
+        booksHTML += bookHTML;
+    }
+    bookWrapper.innerHTML = booksHTML;
+}
+
+const favoriteBooks = [];
+function initActions(){
+    const imgs = document.querySelectorAll('.books-list .book__image');
+    const imgId = document.querySelector('#data-id');
+        for(let img of imgs){
+            img.addEventListener('dblclick', function(event){
+                event.preventDefault();
+                const imgId = img.getAttribute('data-id');
+
+                if(!favoriteBooks.includes(imgId)){
+                img.classList.add('favorite');
+                favoriteBooks.push(imgId);
+                }else{
+                    img.classList.remove('favorite');
+                    const index = favoriteBooks.indexOf(imgId);
+                    if (index !== -1) {
+                        favoriteBooks.splice(index, 1);
+                        console.log('Id removed from array', favoriteBooks);
+                    }
+                }
+            });
+        }
+    console.log(favoriteBooks);
+}
+
+renderBooks();
+initActions();
